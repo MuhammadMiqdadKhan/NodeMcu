d4de76e054d7670f54b5c744445cbeb6490515cf
Muhammad Miqdad Khan <49819879+MuhammadMiqdadKhan@users.noreply.github.com>
2020-04-08 18:26:09 +0500

    Add files via upload

---
+++ blink_LED_program_IotWS/blink_LED_program_IotWS.ino
@@ -0,0 +1,17 @@
+
+/* IoT Workshop 27,28 Nov (Day1), UIT */
+/* Blink LED program for Node MCU */
+
+//the setup function runs once when you press reset or power the board
+void setup() {
+  pinMode(D4, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(D4, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);                       // wait for a second
+  digitalWrite(D4, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);                       // wait for a second
+}
+
